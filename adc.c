7d947cb2b572a74db63648cd02b564913031c152
Zsolt <kzsolt3141@gmail.com>
2019-12-18 23:56:37 +0200

    Replace tabs with spaces

--- adc.c
+++ adc.c
@@ -10,24 +10,24 @@
 #include <avr/interrupt.h>
 
 void ADCInit(uint8_t pin, uint8_t isrEn) {
-	cli();
-	ADMUX  |= (0     << REFS1) | (1     << REFS0) | // voltage ref = AVCC 
-	          pin;                                  // select pin for ADC
-	ADCSRA |= (1     << ADEN) |                     // enable ADC
-		      (isrEn << ADIE) |                     // ADC interrupt enable
-			  (isrEn << ADSC);						// start a new conversion if interrupt is enabled
-	DDRC = 1 << pin;                                // set ADC pin as input
-	rawADC = 0;
-	sei();
+    cli();
+    ADMUX  |= (0     << REFS1) | (1     << REFS0) | // voltage ref = AVCC 
+              pin;                                  // select pin for ADC
+    ADCSRA |= (1     << ADEN) |                     // enable ADC
+              (isrEn << ADIE) |                     // ADC interrupt enable
+              (isrEn << ADSC);						// start a new conversion if interrupt is enabled
+    DDRC &= ~(1 << pin);                                // set ADC pin as input
+    rawADC = 0;
+    sei();
 }
 
 uint16_t ADCRead() {
-	ADCSRA |= (1<<ADSC);                            // Start conversion
+    ADCSRA |= (1<<ADSC);                            // Start conversion
     while (ADCSRA & (1<<ADSC));                     // wait for conversion to complete
     return ADC;                                     // Store ADC value	
 }
 
 ISR(ADC_vect) {
-	rawADC = ADC;                                   // save ADC value in global variable
-	ADCSRA |= (1<<ADSC);                            // start a new conversion
+    rawADC = ADC;                                   // save ADC value in global variable
+    ADCSRA |= (1<<ADSC);                            // start a new conversion
 }
