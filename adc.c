85e815759b3aaea70973f2e67a531318b56a2efc
Zsolt <kzsolt3141@gmail.com>
2021-12-08 18:24:17 +0200

    ADC code refactor

--- adc.c
+++ adc.c
@@ -6,28 +6,63 @@
  */ 
 
 #include "adc.h"
+
+#include <stddef.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void ADCInit(uint8_t pin, uint8_t isrEn) {
-    cli();
-    ADMUX  |= (0     << REFS1) | (1     << REFS0) | // voltage ref = AVCC 
-              pin;                                  // select pin for ADC
-    ADCSRA |= (1     << ADEN) |                     // enable ADC
-              (isrEn << ADIE) |                     // ADC interrupt enable
-              (isrEn << ADSC);						// start a new conversion if interrupt is enabled
-    DDRC &= ~(1 << pin);                                // set ADC pin as input
-    rawADC = 0;
-    sei();
+static ADC_isr_cb  ADC_cb_     = NULL;
+static void       *ADC_cb_ctx_ = NULL;
+static uint8_t     isr_set_    = 0;
+
+void regiter_ADC_isr_cb(ADC_isr_cb cb, void* ctx) {
+    if (cb) {
+        ADC_cb_ = cb;
+    } else {
+        return;
+    }
+
+    ADC_cb_ctx_ = ctx;
+    isr_set_ = 1;
 }
 
-uint16_t ADCRead() {
-    ADCSRA |= (1<<ADSC);                            // Start conversion
-    while (ADCSRA & (1<<ADSC));                     // wait for conversion to complete
-    return ADC;                                     // Store ADC value	
+uint8_t ADC_pin_init(
+    uint8_t          pin,
+    ADC_clock_source clk_src,
+    uint8_t          en_free_run,
+    uint8_t          en_isr
+) {
+    cli();
+
+    ADMUX  |= (0 << REFS1) |
+              (1 << REFS0) | // voltage ref = AVCC 
+              pin;               // select pin for ADC
+    ADCSRA |= (1 << ADEN) |  // enable ADC
+              (1 << ADSC) |  // start conversion
+              (en_free_run << ADFR) |   // free running mode
+              clk_src;
+
+    if (en_isr) {
+        if (! isr_set_) return 1;
+        ADCSRA |= (1     << ADIE);   // enable ADC interrup
+    }
+
+    DDRC &= ~(1 << pin);         // set ADC pin as input
+
+    sei();
+
+    return 0;
+}
+
+uint16_t adc_single_run() {
+    ADCSRA |= (1 << ADSC);               // start a conversion
+    if (ADCSRA & (1 << ADIE)) return 0;  // value will be delivered in ISR
+
+    while (ADCSRA & (1 << ADSC));  // wait for conversion to complete
+    return ADC;                    // Store ADC value
 }
 
 ISR(ADC_vect) {
-    rawADC = ADC;                                   // save ADC value in global variable
-    ADCSRA |= (1<<ADSC);                            // start a new conversion
+    ADC_cb_(ADC_cb_ctx_);
 }
--- adc.h
+++ adc.h
@@ -10,22 +10,51 @@
 
 #include <stdint.h>
 
+typedef enum ADC_clock_source_t {
+    ADC_PS_DEFAULT_2 = 0 ,
+    ADC_PS_2,
+    ADC_PS_4,
+    ADC_PS_8,
+    ADC_PS_16,
+    ADC_PS_32,
+    ADC_PS_64,
+    ADC_PS_128
+} ADC_clock_source;
+
+/**
+ * ADC interrupt callback type
+ * @param[inout] ctx user data for interrupt callback
+ * When ISR occurs ADC_isr_cb will be called with ctx as parameter
+ */
+typedef void (*ADC_isr_cb)(void* ctx);
+
+/**
+ * Register callback and context for ADC interrupt
+ * @param[in] cb   callback for isr; must not be NULL
+ * @param[in] ctx  user defined callback context; must not be NULL
+ * 
+ */
+void regiter_ADC_isr_cb(ADC_isr_cb cb, void* ctx);
+
 /**
  * Initialize ADC for voltage ref AVCC, connect AREF to GND with C=100nF
- * @param [in] pin   select which pin of port C to be used for ADC
- * @param [in] isrEn enable ADC read by ISR, value is saved in rawADC
+ * this mode will use one ADC pin for single run conversion
+ * @param [in] pin         select which pin of port C to be used for ADC
+ * @param [in] clk_src     select clock prescaler, see ADC_clock_source_t
+ * @param [in] en_free_run ADC vill measure in continious mode
+ * @param [in] en_isr      enable ADC read by ISR, value is saved in rawADC
  */
-void ADCInit(uint8_t pin, uint8_t isrEn);
+uint8_t ADC_pin_init(
+    uint8_t          pin,
+    ADC_clock_source clk_src,
+    uint8_t          en_free_run,
+    uint8_t          en_isr
+);
 
 /**
- * Read ADC output register
- * @return ADC value (10 bit precision)
+ * Initialize or read one ADV value.
+ * This can be used to read one adc value if free-run mode is disabled
  */
-uint16_t ADCRead();
-
-/**
- * Global variable to store ADC from ISR.
- */
-uint16_t rawADC;
+uint16_t adc_single_run();
 
 #endif /* ADC_TOOLS_H_ */
